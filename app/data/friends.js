101da4fa6d8f2d5af3e33b9889eafc73315e17d5
Ashley MacWhirter <macwhirtera@gmail.com>
2017-04-23 23:40:18 -0400

    api routing, base friends api array

---
+++ app/data/friends.js
@@ -0,0 +1,31 @@
+//create array of default friends
+
+var friends = [{
+  name: "John Torpey",
+  photo: "https://cdn.pixabay.com/photo/2016/05/23/23/32/human-1411499_1280.jpg",
+  scores: [3, 1, 4, 4, 2, 4, 5, 2, 4, 2]
+}, {
+  name: "Laura Toner",
+  photo: "https://cdn.pixabay.com/photo/2015/02/13/14/32/apples-635240_1280.jpg",
+  scores: [2, 5, 3, 4, 5, 3, 1, 3, 2, 4]
+}, {
+  name: "Bob Merad",
+  photo: "https://cdn.pixabay.com/photo/2016/09/24/03/20/passion-1690965_1280.jpg",
+  scores: [4, 3, 3, 2, 1, 2, 5, 4, 2, 1]
+},{
+  name: "Amy Crosby",
+  photo: "https://cdn.pixabay.com/photo/2017/03/17/04/07/beautiful-2150881_1280.jpg",
+  scores: [1, 2, 3, 3, 4, 1, 2, 5, 3, 3]
+}, {
+  name: "Chris Maclin",
+  photo: "https://cdn.pixabay.com/photo/2015/04/29/22/17/human-746195_1280.jpg",
+  scores: [5, 4, 5, 3, 2, 4, 2, 1, 3, 2]
+}, {
+  name: "Jack Waller",
+  photo: "https://cdn.pixabay.com/photo/2014/11/04/09/52/drive-516376_1280.jpg",
+  scores: [2, 2, 1, 3, 4, 2, 5, 3, 3, 4]
+}];
+
+ //export array
+
+module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,8 +1,14 @@
 var path = require("path");
+var friends = require("../data/friends");
 
 module.exports = function(app) {
 
   app.get("/api/friends", function(req, res) {
     res.json(friends);
+  });
+
+  // Create New Reservation - takes in JSON input
+  app.post("/api/new", function(req, res) {
+    var newFriend= req.body;
   });
 };
