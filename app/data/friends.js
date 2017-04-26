f873724aab1bcdc8c2f6c41d942b84830414d63e
Ashley MacWhirter <macwhirtera@gmail.com>
2017-04-26 18:42:44 -0400

    app working - need cleanup and styling

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,27 +2,31 @@
 
 var friends = [{
   name: "John Torpey",
-  photo: "https://cdn.pixabay.com/photo/2016/05/23/23/32/human-1411499_1280.jpg",
+  photo: "https://static.pexels.com/photos/343717/pexels-photo-343717.jpeg",
   scores: [3, 1, 4, 4, 2, 4, 5, 2, 4, 2]
 }, {
   name: "Laura Toner",
-  photo: "https://cdn.pixabay.com/photo/2015/02/13/14/32/apples-635240_1280.jpg",
+  photo: "https://static.pexels.com/photos/206481/pexels-photo-206481.jpeg",
   scores: [2, 5, 3, 4, 5, 3, 1, 3, 2, 4]
 }, {
   name: "Bob Merad",
-  photo: "https://cdn.pixabay.com/photo/2016/09/24/03/20/passion-1690965_1280.jpg",
+  photo: "https://static.pexels.com/photos/249760/pexels-photo-249760.jpeg",
   scores: [4, 3, 3, 2, 1, 2, 5, 4, 2, 1]
 },{
   name: "Amy Crosby",
-  photo: "https://cdn.pixabay.com/photo/2017/03/17/04/07/beautiful-2150881_1280.jpg",
+  photo: "https://static.pexels.com/photos/270308/pexels-photo-270308.jpeg",
   scores: [1, 2, 3, 3, 4, 1, 2, 5, 3, 3]
 }, {
   name: "Chris Maclin",
-  photo: "https://cdn.pixabay.com/photo/2015/04/29/22/17/human-746195_1280.jpg",
+  photo: "https://static.pexels.com/photos/249757/pexels-photo-249757.jpeg",
   scores: [5, 4, 5, 3, 2, 4, 2, 1, 3, 2]
 }, {
+  name: "Anna Schultz",
+  photo: "https://static.pexels.com/photos/388517/pexels-photo-388517.jpeg",
+  scores: [2, 1, 1, 2, 5, 4, 3, 3, 1, 4]
+}, {
   name: "Jack Waller",
-  photo: "https://cdn.pixabay.com/photo/2014/11/04/09/52/drive-516376_1280.jpg",
+  photo: "https://static.pexels.com/photos/64385/pexels-photo-64385.jpeg",
   scores: [2, 2, 1, 3, 4, 2, 5, 3, 3, 4]
 }];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,14 +1,37 @@
 var path = require("path");
 var friends = require("../data/friends");
 
-module.exports = function(app) {
+module.exports = function(app){
 
-  app.get("/api/friends", function(req, res) {
-    res.json(friends);
-  });
+    app.get('/api/friends', function(req, res){
+        console.log("Reading API");
+        res.json(friends);
+    });
 
-  // Create New Reservation - takes in JSON input
-  app.post("/api/new", function(req, res) {
-    var newFriend= req.body;
-  });
+    app.post("/api/new", function(req, res) {
+      //setup variables for finding match
+        var newFriend = req.body;
+        var myScore = newFriend.scores;
+        var total = 0;
+        var bestMatch = 1000;
+        var index = -1;
+
+        for(var i=0; i<friends.length; i++){
+            //Iterate through the whole list of friends already in database
+            total = 0;
+
+            for(var j=0; j<myScore.length; j++){
+                //for each friend calculate the total value
+                var diff = Math.abs(myScore[j] - friends[i].scores[j]);
+                total += diff;
+            }
+            if(total < bestMatch){
+                bestMatch = total;
+                index = i;
+            }
+        }
+        console.log('Best Match:', friends[index]);
+        friends.push(newFriend);
+        res.json(friends[index]);
+    });
 };
