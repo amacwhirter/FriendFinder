e38378f23a20576cf1e9149e2e78e8b8e9f4d86b
Ashley MacWhirter <macwhirtera@gmail.com>
2017-04-28 18:46:43 -0400

    static add

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ var PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 
 app.use(express.static(__dirname + "/app/public"));
+app.use( '/app', express.static( 'app' ) );
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
