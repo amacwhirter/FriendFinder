6ecba8ddc36f7d91fedef56ac80b8e3c683a1012
Ashley MacWhirter <macwhirtera@gmail.com>
2017-04-29 09:59:50 -0400

    heroku fix

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,7 +2,7 @@ var path = require("path");
 
 module.exports = function(app){
 
-	app.get('/', function (req, res) {
+	app.use('/', function (req, res) {
 		res.sendFile(path.join(__dirname, "../public/home.html"));
 	});
 
